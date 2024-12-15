8d9508928b13ab4329309615ea51d08d9fcc4967
DnyaNvB <nvbdnya@gmail.com>
2024-12-15 17:58:43 +0330

    add Facade Pattern implementation for Parser

--- src/main/java/MiniJava/Main.java
+++ src/main/java/MiniJava/Main.java
@@ -4,17 +4,17 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 import MiniJava.errorHandler.ErrorHandler;
-import MiniJava.parser.Parser;
+import MiniJava.parser.ParserFacade;
 
 
 public class Main {
     public static void main(String[] args) {
-        Parser parser = new Parser();
+        ParserFacade parserFacade = new ParserFacade();
         try {
             // start parsing
-            parser.startParse(new Scanner(new File("src/main/resources/code")));
+            parserFacade.parseInput(new Scanner(new File("src/main/resources/code")));
         } catch (FileNotFoundException e) {
-            ErrorHandler.printError(e.getMessage());
+            ErrorHandler.printError("file not found: " + e.getMessage());
         }
     }
 }
---
+++ src/main/java/MiniJava/parser/ParserFacade.java
@@ -0,0 +1,20 @@
+package MiniJava.parser;
+import MiniJava.errorHandler.ErrorHandler;
+
+import java.util.Scanner;
+
+public class ParserFacade {
+    private final Parser parser;
+
+    public ParserFacade() {
+        this.parser = new Parser();
+    }
+
+    public void parseInput(Scanner inputScanner) {
+        try {
+            parser.startParse(inputScanner);
+        } catch (Exception e) {
+            ErrorHandler.printError("An error occurred during parsing: " + e.getMessage());
+        }
+    }
+}
